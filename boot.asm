34ea36ee82f959f863430e2b723409671703d4e4
cto-semiquantum <harsh@semiquantum.live>
2026-05-15 18:00:06 +0530

    Added text rendering to bootloader

--- boot.asm
+++ boot.asm
@@ -1,14 +1,21 @@
 [org 0x7c00]
 
-mov ah, 0x0e
+mov si, message
 
-mov al, 'S'
-int 0x10
+print:
+    lodsb
+    or al, al
+    jz done
 
-mov al, 'Q'
-int 0x10
+    mov ah, 0x0e
+    int 0x10
 
-jmp $
+    jmp print
+
+done:
+    jmp $
+
+message db 'WELCOME TO SQ OS', 0
 
 times 510-($-$$) db 0
 dw 0xaa55
